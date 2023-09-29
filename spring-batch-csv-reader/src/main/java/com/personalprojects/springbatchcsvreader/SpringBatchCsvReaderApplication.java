cf03b64e57e2864caf2a70f92b349c0bf4a44b40
dasari manoj <dasarimanoj265@gmail.com>
2023-09-29 08:57:27 +0530

    Initial Commit

--- spring-batch-csv-reader/src/main/java/com/personalprojects/springbatchcsvreader/SpringBatchCsvReaderApplication.java
+++ spring-batch-csv-reader/src/main/java/com/personalprojects/springbatchcsvreader/SpringBatchCsvReaderApplication.java
@@ -3,7 +3,6 @@ package com.personalprojects.springbatchcsvreader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class SpringBatchCsvReaderApplication {
 
 	public static void main(String[] args) {
